f6b015a9455080390f6e97a2f6e3be7b401e7c8f
Abdelrhman-m-Reda <zainmalk979@gmail.com>
2023-06-25 11:08:35 -0700

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - fuction that computes the absolue
+ * 	value of an integer
+ *
+ * @n: takes in integer type
+ *
+ * Return: Always 0 (success)
+*/
+
+int _abs(int n)
+{
+	if (n < 0)
+		n = (-1) * n;
+	return (n);
+}
